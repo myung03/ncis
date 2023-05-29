bcef7ba4d7ebcb71a9df941ff549eab0e92909d7
Matthew <matthewyungisworking@gmail.com>
2023-05-29 14:29:59 -0700

    added main

--- ncis/src/main.jsx
+++ ncis/src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import './fonts/Beckman.woff2';
-import './fonts/montserrat.woff2';
+import './fonts/Beckman/Beckman.woff2';
+import './fonts/Montserrat/montserrat.woff2'
+import { ChakraProvider } from '@chakra-ui/react'
+import * as ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ChakraProvider>
     <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
