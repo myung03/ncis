d1e2cbf3cec5d00ae1bee9b1109d754df168d65d
Janaye Cheong <janaye.c13@gmail.com>
2023-05-29 14:07:48 -0700

    added chakra and fonts

--- ncis/src/App.jsx
+++ ncis/src/App.jsx
@@ -3,10 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import * as React from 'react'
+
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from '@chakra-ui/react'
 function App() {
 
   return (
     <div>
+    <ChakraProvider>
+    </ChakraProvider>
       NCIS 
     </div>
   )
--- ncis/src/main.jsx
+++ ncis/src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './fonts/Beckman.woff2';
+import './fonts/montserrat.woff2';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
